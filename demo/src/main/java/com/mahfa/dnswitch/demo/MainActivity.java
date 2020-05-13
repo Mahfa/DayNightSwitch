025dfb2da257cc5098fb5ab87be281992a9844f0
Quentin <vfishv@gmail.com>
2020-05-13 10:39:14 +0800

    androidx

--- demo/src/main/java/com/mahfa/dnswitch/demo/MainActivity.java
+++ demo/src/main/java/com/mahfa/dnswitch/demo/MainActivity.java
@@ -1,7 +1,6 @@
 package com.mahfa.dnswitch.demo;
 
-import android.os.PersistableBundle;
-import android.support.v7.app.AppCompatActivity;
+import androidx.appcompat.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
